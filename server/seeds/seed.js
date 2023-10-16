22da8db44d09a0a35614017801f7ac3f12fea8e2
Niko Auer <nikoauer26@gmail.com>
2023-10-16 18:28:36 +1100

    adding json data

---
+++ server/seeds/seed.js
@@ -0,0 +1,8 @@
+const db = require('../config/connection');
+const { Shoes } = require('../models/index')
+
+const ShoeData = require('./ShoeData');
+
+db.once('open', async () => {
+    
+})
--- server/server.js
+++ server/server.js
@@ -1,15 +1,30 @@
-const express = require('express');
-const db = require('./config/connection');
+const express = require("express");
+const db = require("./config/connection");
+const { typeDefs, resolvers } = require("./schema/index");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const startApolloServer = async () => {
+  await server.start();
 
-db.once('open', () => {
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  app.use('/graphql', expressMiddleware(server));
+
+  db.once("open", () => {
     app.listen(PORT, () => {
-      console.log(`Now listening on localhost:${PORT}`);
+      console.log(`Now listening on localhost:${PORT}`)
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
     });
   });
-  
+};
+
+startApolloServer();
