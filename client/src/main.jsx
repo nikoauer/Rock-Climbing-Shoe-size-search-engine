5006784626e9394ca80552a3cd0a232bbe22ca40
Niko Auer <nikoauer26@gmail.com>
2023-10-20 14:46:27 +1100

    added dom router for react

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Home from './pages/SearchShoes'
+import SavedShoes from './pages/SavedShoes'
+import Error from './pages/Error'
+
 import App from './App.jsx'
-import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/savedshoes',
+        element: <SavedShoes />,
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
---
+++ client/src/pages/SearchShoes.jsx
@@ -0,0 +1,3 @@
+export default function SearchShoes() {
+    <h1>hellow world</h1>
+}
